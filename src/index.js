2d6961a821c9cbef8788ed7f4f0c8c2eaf3af719
Marius Andra <marius.andra@gmail.com>
2016-03-13 14:05:02 +0100

    very basic example app

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+// import App from 'containers/App';
+// import store from 'store';
+
+import 'index.html';
+// <Provider store={store}>
+//   <App />
+// </Provider>,
+
+ReactDOM.render(
+  <div>Rendered by react!</div>,
+  document.getElementById('root')
+);
