ccd67e733466ed2819b91c9736cd0c2b3e50b0df
Marius Andra <marius.andra@gmail.com>
2016-04-20 11:08:04 +0200

    Revert "clean up store creation"
    This reverts commit ec4fd3d0221ef9d564a0f1314d5ef8112a8ef421.

--- src/store.js
+++ src/store.js
@@ -1,25 +1,84 @@
 /* global window */
 
-import { createStore, applyMiddleware, compose } from 'redux'
+// selector
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import createSagaMiddleware from 'redux-saga'
+import { take, cancel, fork, call } from 'redux-saga/effects'
 import { routerMiddleware } from 'react-router-redux'
 import { browserHistory } from 'react-router'
-import { createRootSaga, createKeaStore } from 'kea-logic'
 
-function * appSaga () {
+export const NEW_SCENE = '@@kea/NEW_SCENE'
+
+let loadedReducers = {}
+let loadedWorkers = {}
+let currentScene = null
+
+const createRootSaga = function (appSagas = null) {
+  return function * () {
+    let runningSaga = null
+
+    if (appSagas) {
+      yield call(appSagas)
+    }
+
+    while (true) {
+      const { payload } = yield take(NEW_SCENE)
+
+      if (runningSaga) {
+        yield cancel(runningSaga)
+      }
+
+      if (loadedWorkers[payload.name]) {
+        runningSaga = yield fork(loadedWorkers[payload.name])
+      }
+    }
+  }
 }
 
-const appReducers = {
-  routing: routerReducer
-}
-
+const sagaMiddleware = createSagaMiddleware(createRootSaga(null))
 const finalCreateStore = compose(
-  applyMiddleware(createSagaMiddleware(createRootSaga(appSaga))),
-  applyMiddleware(routerMiddleware(browserHistory)),
+  applyMiddleware(sagaMiddleware),
+  applyMiddleware(
+    routerMiddleware(browserHistory)
+  ),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 )(createStore)
 
-const store = createKeaStore(finalCreateStore, appReducers)
+function createReducer (scenes) {
+  const hasScenes = scenes && Object.keys(scenes).length > 0
+
+  return combineReducers({
+    // form: formReducer,
+    routing: routerReducer,
+
+    scenes: hasScenes ? combineReducers(scenes) : () => ({})
+  })
+}
+
+const rootReducer = createReducer(loadedReducers)
+
+const store = finalCreateStore(rootReducer)
+
+store.addKeaScene = function (scene) {
+  const { name } = scene
+
+  if (currentScene === name) {
+    return
+  }
+
+  loadedReducers[name] = scene.reducer
+  loadedWorkers[name] = scene.worker
+  this.replaceReducer(createReducer(loadedReducers))
+
+  this.dispatch({
+    type: NEW_SCENE,
+    payload: {
+      name
+    }
+  })
+
+  currentScene = name
+}
 
 export default store
