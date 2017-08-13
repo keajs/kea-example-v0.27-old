097cdef09ba27fca4e32d9acf98538b1d2d006ee
Marius Andra <marius.andra@gmail.com>
2017-08-13 22:50:30 +0200

    simplify header

--- app/components/header/index.js
+++ app/components/header/index.js
@@ -1,32 +1,16 @@
 import './styles.scss'
 
 import React, { Component } from 'react'
-import { connect } from 'kea'
 
-import { push } from 'react-router-redux'
+import { NavLink } from 'react-router-dom'
 
-const routeSelector = state => state.router.location
-
-@connect({
-  props: [
-    routeSelector, [
-      'pathname'
-    ]
-  ]
-})
 export default class Header extends Component {
   render () {
-    const { dispatch, pathname } = this.props
-
-    function load (url) {
-      return (event) => { event.preventDefault(); dispatch(push(url)) }
-    }
-
     return (
       <header className='body-header'>
         <nav>
-          <a href='/' onClick={load('/')} className={pathname === '/' ? 'active' : ''}>Kea example</a>
-          <a href='/todos' onClick={load('/todos')} className={pathname.indexOf('/todos') === 0 ? 'active' : ''}>Todos</a>
+          <NavLink to='/' exact>Kea example</NavLink>
+          <NavLink to='/todos'>Todos</NavLink>
 
           <a className='right' href='https://www.github.com/keajs/kea-example' target='_blank'>Fork on Github</a>
         </nav>
