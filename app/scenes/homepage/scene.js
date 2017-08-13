f5578a05c9ea2f04068fdadb7e917b3245b7006e
Marius Andra <marius.andra@gmail.com>
2017-08-13 22:53:50 +0200

    delete old scene files

--- app/scenes/homepage/scene.js
+++
@@ -1,8 +0,0 @@
-import { createScene } from 'kea/scene'
-
-import sceneComponent from '~/scenes/homepage/index'
-
-export default createScene({
-  name: 'homepage',
-  component: sceneComponent
-})
--- app/scenes/todos/scene.js
+++
@@ -1,8 +0,0 @@
-import { createScene } from 'kea/scene'
-
-import sceneComponent from '~/scenes/todos/index'
-
-export default createScene({
-  name: 'todos',
-  component: sceneComponent
-})
